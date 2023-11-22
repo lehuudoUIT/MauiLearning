4df080808c5c998e8b440c04e01731537a5d2968
Dolh <22520254@gm.uit.edu.vn>
2023-11-22 14:12:25 +0700

    ViewCell + Binding from CS

--- Project1/Project1/Pages/ListView/ListViewDemo.xaml.cs
+++ Project1/Project1/Pages/ListView/ListViewDemo.xaml.cs
@@ -1,3 +1,4 @@
+using Project1.Pages.CarouselView.Models;
 namespace Project1.Pages.ListView;
 
 public partial class ListViewDemo : ContentPage
@@ -5,5 +6,37 @@ public partial class ListViewDemo : ContentPage
 	public ListViewDemo()
 	{
 		InitializeComponent();
+
+		var items = new List<CollectionItem>
+		{
+			new CollectionItem
+			{
+				Title = "Item 1",
+				Description = "This is item 1"
+			},
+            new CollectionItem
+            {
+                Title = "Item 2",
+                Description = "This is item 2"
+            },
+            new CollectionItem
+            {
+                Title = "Item 3",
+                Description = "This is item 3"
+            },
+            new CollectionItem
+            {
+                Title = "Item 4",
+                Description = "This is item 4"
+            },
+            new CollectionItem
+            {
+                Title = "Item 5",
+                Description = "This is item 5"
+            }
+        };
+
+        listView.ItemsSource = items;
+
 	}
 }
